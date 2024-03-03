d8fad3ad892ab9d83d2d1f9c0ac0160b54c3d28b
davit@aspire3 <khanaldavit@gmail.com>
2024-03-03 23:54:14 +0545

    added form validation

--- src/app/api/s3-upload/route.js
+++ src/app/api/s3-upload/route.js
@@ -1,34 +1,44 @@
 import { NextResponse } from "next/server";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
-const sclient = new S3Client([
-  {
-    region: process.env.NEXT_PUBLIC_AWS_S3_REGION,
-    credentials: {
-      accessKeyId: process.env.NEXT_PUBLIC_AWS_S3_ACCES_KEY,
-      secretAccessKey: process.env.NEXT_PUBLIC_AWS_S3_SECRET_KEY,
-    },
+const s3Client = new S3Client({
+  region: process.env.NEXT_AWS_S3_REGION,
+  credentials: {
+    accessKeyId: process.env.NEXT_AWS_S3_ACCES_KEY,
+    secretAccessKey: process.env.NEXT_AWS_S3_SECRET_KEY,
   },
-]);
+});
 
 async function uploadFileToS3(file, fileName) {
   const fileBuffer = file;
   console.log(fileName);
+
+  const params = {
+    Bucket: process.env.NEXT_AWS_S3_BUCKET,
+    Key: `${fileName}`,
+    Body: fileBuffer,
+    ContentType: "image/jpg",
+  };
+
+  const command = new PutObjectCommand(params);
+  await s3Client.send(command);
+  return fileName;
 }
+
 export async function POST(request) {
   try {
     const formData = await request.formData();
     const file = formData.get("file");
 
-    //file check
     if (!file) {
-      return NextResponse.json({ error: "File not present" });
+      return NextResponse.json({ error: "File is required." }, { status: 400 });
     }
 
     const buffer = Buffer.from(await file.arrayBuffer());
-    const fileName = await uploadFileToS3(file.name);
-    return NextResponse.json({ success: "Hello got file" });
+    const fileName = await uploadFileToS3(buffer, file.name);
+
+    return NextResponse.json({ success: true, fileName });
   } catch (error) {
-    return NextResponse.json({ error: "Hello got error" });
+    return NextResponse.json({ error });
   }
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { NextResponse } from "next/server";
+import S3UploadForm from "@/app/components/S3UploadForm";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      Hehe better add /s3-upload in link above here
+      <S3UploadForm />
     </main>
   );
 }
